cfbae2cc7b990c00665dadb3eb0adf6aee93da1e
Louis Heath <lheath1998@gmail.com>
2023-12-01 18:28:51 +0000

    part one complete

--- 1_trebuchet/trebuchet.ex
+++ 1_trebuchet/trebuchet.ex
@@ -1,6 +1,13 @@
 defmodule Trebuchet do
-  def extractDigits(line) do
-    1
+  def calibrationValue(line) do
+    num_chars = for n <- String.to_charlist(line), n > 47 and n < 58, do: n
+    num_str = List.to_string(num_chars)
+
+    first = String.first(num_str)
+    last = String.slice(num_str, String.length(num_str) - 1, String.length(num_str))
+
+    {calibration, _} = Integer.parse(first <> last)
+    calibration
   end
 
   def main do
@@ -9,7 +16,7 @@ defmodule Trebuchet do
     sum =
       contents
       |> String.split("\n", trim: true)
-      |> Enum.map(fn line -> 1 end)
+      |> Enum.map(fn line -> calibrationValue(line) end)
       |> Enum.sum()
 
     IO.puts("Sum: #{sum}")
