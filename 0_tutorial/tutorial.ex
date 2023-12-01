c348e94ca25ba7af8646e7f709b327342b572fbe
Louis Heath <lheath1998@gmail.com>
2023-12-01 13:13:41 +0000

    more tutorial

--- 0_tutorial/tutorial.ex
+++ 0_tutorial/tutorial.ex
@@ -48,5 +48,67 @@ defmodule Tutorial do
     IO.puts("4 === 4.0: #{4 === 4.0}")
     IO.puts("4 != 4.0: #{4 != 4.0}")
     IO.puts("4 !== 4.0: #{4 !== 4.0}")
+
+    # >, <, <=, >= are as usual
+    age = 16
+    IO.puts("Vote & Drive: #{age >= 16 and age >= 18}")
+    IO.puts(not true)
+  end
+
+  def branching do
+    age = 16
+
+    if age >= 18 do
+      IO.puts("Can vote")
+    else
+      IO.puts("Can't vote")
+    end
+
+    unless age === 18 do
+      IO.puts("Not 18")
+    else
+      IO.puts("Is 18")
+    end
+
+    # like a big if, else if, ...
+    cond do
+      age >= 18 -> IO.puts("You can vote")
+      age >= 16 -> IO.puts("You can drive")
+      age >= 14 -> IO.puts("You need to wait")
+      true -> IO.puts("Default")
+    end
+
+    # like a switch statement
+    case 2 do
+      1 -> IO.puts("One")
+      2 -> IO.puts("Two")
+      _ -> IO.puts("Default")
+    end
+
+    IO.puts("Ternary: #{if age > 18, do: "Can vote", else: "Can't vote"}")
+  end
+
+  def tuples do
+    # normally contain 2-4 values. not for enumeration (looping)
+    my_stats = {175, 6.25, :MyAtom}
+
+    IO.puts("Tuple?: #{is_tuple(my_stats)}")
+
+    # all variables are constant
+    my_stats2 = Tuple.append(my_stats, 42)
+
+    IO.puts("Age: #{elem(my_stats2, 3)}")
+
+    IO.puts("Size: #{tuple_size(my_stats2)}")
+
+    my_stats3 = Tuple.delete_at(my_stats2, 0)
+
+    my_stats4 = Tuple.insert_at(my_stats3, 0, 1974)
+    IO.puts("Mutated: #{Enum.join(my_stats4)}")
+
+    many_zeroes = Tuple.duplicate(0, 5)
+
+    {weight, height, name} = {175, 6.25, "MyAtom"}
+    IO.puts("Weight: #{weight}")
   end
 end
