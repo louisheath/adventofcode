9d2a169e8ed9b5f12bfe9fa69a660c2cad4fd0e7
Louis Heath <lheath1998@gmail.com>
2023-12-01 16:01:44 +0000

    some more tutorial

--- 0_tutorial/tutorial.ex
+++ 0_tutorial/tutorial.ex
@@ -104,11 +104,148 @@ defmodule Tutorial do
     my_stats3 = Tuple.delete_at(my_stats2, 0)
 
     my_stats4 = Tuple.insert_at(my_stats3, 0, 1974)
-    IO.puts("Mutated: #{Enum.join(my_stats4)}")
+    IO.puts("Size: #{tuple_size(my_stats2)}")
 
     many_zeroes = Tuple.duplicate(0, 5)
 
     {weight, height, name} = {175, 6.25, "MyAtom"}
     IO.puts("Weight: #{weight}")
+  end
+
+  def lists do
+    list1 = [1, 2, 3]
+    list2 = [4, 5, 6]
+
+    list3 = list1 ++ list2
+
+    list4 = list3 -- list1
+
+    IO.puts(6 in list4)
+
+    [head | tail] = list3
+    IO.puts("Head: #{head}")
+
+    # no newline
+    IO.write("Tail: ")
+
+    IO.inspect(tail)
+
+    Enum.each(tail, fn item -> IO.puts(item) end)
+
+    words = ["Random", "Words", "in a", "list"]
+    Enum.each(words, fn word -> IO.puts(word) end)
+
+    recursive(words)
+    recursive(List.delete(words, "Random"))
+    recursive(List.delete_at(words, 1))
+
+    # list of key value tuple. like a map?
+    my_stats = [name: "Louis", height: 182]
+    IO.puts(my_stats[:name])
+  end
+
+  def recursive([word | words]) do
+    IO.puts(word)
+    recursive(words)
+  end
+
+  def recursive([]), do: nil
+
+  def maps do
+    capitals = %{"Alabama" => "Montgomery", "Alaska" => "Juneau", "Arizona" => "Phoenix"}
+
+    IO.puts("Capital of Alaska is #{capitals["Alaska"]}")
+
+    # using atoms
+    capitals2 = %{alabama: "Montgomery", alaska: "Juneau", arizona: "Phoenix"}
+
+    IO.puts("Capital of Arizona is #{capitals2.arizona}")
+
+    capitals3 = Dict.put_new(capitals, "Arkansas", "Little Rock")
+  end
+
+  def pattern_matching do
+    # retrieve data from different types of data structures
+    [length, width] = [20, 30]
+    IO.puts("Width: #{width}")
+
+    [_, [_, a]] = [20, [30, 40]]
+    IO.puts("Useless number: #{a}")
+  end
+
+  def anon_funcs do
+    get_sum = fn x, y -> x + y end
+
+    # whytf do I need a dot
+    IO.puts("5 + 5 = #{get_sum.(5, 5)}")
+
+    get_less = &(&1 - &2)
+    IO.puts("7 - 3 = #{get_less.(7, 3)}")
+
+    add_sum = fn
+      {x, y} -> IO.puts("#{x} + #{y} = #{x + y}")
+      {x, y, z} -> IO.puts("#{x} + #{y} + #{z} = #{x + y + z}")
+    end
+
+    add_sum.({1, 2})
+    add_sum.({1, 2, 3})
+
+    IO.puts(do_it())
+  end
+
+  # default values
+  def do_it(x \\ 1, y \\ 1) do
+    x + y
+  end
+
+  # recursion is used for looping as variables are immutable
+  def factorial(num) do
+    if num <= 1 do
+      1
+    else
+      num * factorial(num - 1)
+    end
+  end
+
+  def sum([]), do: 0
+
+  def sum([head | tail]), do: head + sum(tail)
+
+  def loop(max, min) do
+    if max < min do
+      nil
+    else
+      IO.puts("Number: #{max}")
+      loop(max - 1, min)
+    end
+  end
+
+  def enums do
+    IO.puts(
+      "Even list: #{Enum.all?([1, 2, 3],
+      fn n -> rem(n, 2) == 0 end)}"
+    )
+
+    IO.puts(
+      "Any even in list: #{Enum.any?([1, 2, 3],
+      fn n -> rem(n, 2) == 0 end)}"
+    )
+
+    Enum.each([1, 2, 3], fn n -> IO.puts(n) end)
+
+    double_list = Enum.map([1, 2, 3], fn n -> n * 2 end)
+
+    summed = Enum.reduce(double_list, fn n, sum -> n + sum end)
+    IO.puts("Summed: #{summed}")
+
+    IO.inspect(Enum.uniq([1, 2, 2]))
+  end
+
+  def list_comprehensions do
+    doubled = for n <- [1, 2, 3], do: n * 2
+    IO.inspect(doubled)
+
+    even_values = for n <- [1, 2, 3, 4], rem(n, 2) == 0, do: n
+    IO.inspect(even_values)
   end
 end
