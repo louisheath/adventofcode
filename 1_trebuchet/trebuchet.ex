d031a77ec70e4c6d68229815f6c9dd02715ce9c8
Louis Heath <lheath1998@gmail.com>
2023-12-01 19:13:24 +0000

    q2. too high

--- 1_trebuchet/trebuchet.ex
+++ 1_trebuchet/trebuchet.ex
@@ -1,4 +1,42 @@
 defmodule Trebuchet do
+  def numberify(line) do
+    if line === "" do
+      ""
+    else
+      numbers = %{
+        "one" => "1",
+        "two" => "2",
+        "three" => "3",
+        "four" => "4",
+        "five" => "5",
+        "six" => "6",
+        "seven" => "7",
+        "eight" => "8",
+        "nine" => "9"
+      }
+
+      matches =
+        numbers
+        |> Enum.map(fn {k, _} -> k end)
+        |> Enum.filter(fn number ->
+          String.slice(line, 0, String.length(number)) === number
+        end)
+
+      if length(matches) === 1 do
+        number = Enum.at(matches, 0)
+        digit = numbers[number]
+
+        # clean the remainder
+        digit <>
+          numberify(String.slice(line, String.length(number), String.length(line)))
+      else
+        [head | tail] = line
+        # there's no number starting here. check the rest
+        head <> numberify(tail)
+      end
+    end
+  end
+
   def calibrationValue(line) do
     num_chars = for n <- String.to_charlist(line), n > 47 and n < 58, do: n
     num_str = List.to_string(num_chars)
@@ -16,6 +54,8 @@ defmodule Trebuchet do
     sum =
       contents
       |> String.split("\n", trim: true)
+      |> Enum.map(fn line -> numberify(line) end)
+      # |> Enum.map(fn line -> IO.inspect(line) end)
       |> Enum.map(fn line -> calibrationValue(line) end)
       |> Enum.sum()
 
