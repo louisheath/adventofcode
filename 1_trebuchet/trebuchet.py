712984875ed04186d7bece903486ac632a7cd0df
Louis Heath <lheath1998@gmail.com>
2023-12-02 07:25:43 +0000

    pt2 python. add types

--- 1_trebuchet/trebuchet.py
+++ 1_trebuchet/trebuchet.py
@@ -1,43 +1,59 @@
 import sys
 
+numbers = {
+  "one": 1,
+  "two": 2,
+  "three": 3,
+  "four": 4,
+  "five": 5,
+  "six": 6,
+  "seven": 7,
+  "eight": 8,
+  "nine": 9,
+}
+
 class FirstAndLastDigits:
   first = -1
   last = -1
 
-  def track_new_digit(self, digit):
+  def track_new_digit(self, digit: int) -> None:
     if self.first == -1:
       self.first = digit
     self.last = digit
 
-  def final_number(self):
+  def final_number(self) -> int:
     return int(str(self.first) + str(self.last))
 
-def is_digit(char):
+def is_digit(char: int) -> bool:
   return char >= 48 and char <= 57
 
-def matches_word(i, line):
-  return -1
-
-def ascii_to_int(char):
+def ascii_to_int(char: int) -> int:
   return char - 48
 
-def calibration_values(file):
+def matches_word(i: int, line: str) -> [bool, int]:
+  for word in numbers:
+    if line[i:i+len(word)] == word:
+      return [True, numbers[word]]
+  return [False, -1]
+
+def calibration_values(file) -> []:
   vals = []
 
-  for line in file:
+  for line_bytes in file:
+    line: str = line_bytes.decode("utf-8")
     tracker = FirstAndLastDigits()
 
     for i in range(0, len(line)):
-      char = line[i]
+      char: int = line_bytes[i]
 
       if is_digit(char):
         digit = ascii_to_int(char)
         tracker.track_new_digit(digit)
         continue
 
-      matched_digit = matches_word(i, line)
-      if matched_digit != -1:
-        tracker.track_new_digit(matched_digit)
+      [matches, digit] = matches_word(i, line)
+      if matches:
+        tracker.track_new_digit(digit)
         continue
 
     vals.append(tracker.final_number())
