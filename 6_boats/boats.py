79d8ebc8feb293311cf9fa878de384cf1e907cdf
Louis Heath <lheath1998@gmail.com>
2023-12-06 11:12:56 +0000

    day 6 pt 2 python

--- 6_boats/boats.py
+++ 6_boats/boats.py
@@ -10,12 +10,35 @@ def winning_strategies(race: list) -> list:
       strats.append(speed)
   return strats
 
-def part1(races: list) -> int:
+def part1(lines: list) -> int:
+  races = [] # of tuple (time, distance)
+  for t in lines[0][9:].split(" "):
+      if t == "": continue
+      races.append([int(t), 0])
+  i = 0
+  for d in lines[1][9:].split(" "):
+    if d == "": continue
+    races[i][1] = int(d)
+    i += 1
+
   product = 1
   for race in races:
     strats = winning_strategies(race)
     product *= len(strats)
   return product 
+
+def part2(lines: list) -> int:
+  time = ""
+  for t in lines[0][9:].split(" "):
+      if t == "": continue
+      time += t # concat
+  dist = ""
+  for d in lines[1][9:].split(" "):
+    if d == "": continue
+    dist += d
+
+  race = [int(time), int(dist)]
+  return len(winning_strategies(race)) 
 
 def main():
   with open(sys.argv[1], 'rb') as file:
@@ -24,18 +47,11 @@ def main():
       line: str = line_bytes.decode("utf-8").strip("\n")
       lines.append(line)
 
-    races = [] # of tuple (time, distance)
-    for t in lines[0][9:].split(" "):
-       if t == "": continue
-       races.append([int(t), 0])
-    i = 0
-    for d in lines[1][9:].split(" "):
-      if d == "": continue
-      races[i][1] = int(d)
-      i += 1
-
     print("Part 1")
-    print(part1(races))
+    print(part1(lines))
+
+    print("Part 2")
+    print(part1(lines))
 
 if __name__ == "__main__":
     # run with `python3 boats.py input2.txt`
