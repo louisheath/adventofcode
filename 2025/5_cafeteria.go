7e2c1c2264e6209412aa738c0e03b1fe0068d92a
Louis Heath <lheath1998@gmail.com>
2025-12-15 21:17:14 +0000

    day 5 part 2

--- 2025/5_cafeteria.go
+++ 2025/5_cafeteria.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"log"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -9,42 +10,64 @@ import (
 func cafeteria() int {
 	input := readFile("5_input.txt")
 
-	ranges := []idRange{}
-	total := 0
-	processingRanges := true
+	ranges := []*idRange{}
 
 	for _, line := range input {
 		if line == "" {
 			// We've processed all fresh ingredient ranges
-			processingRanges = false
-			continue
+			break
 		}
-
-		if processingRanges {
-			s := strings.Split(line, "-")
-			start, err := strconv.Atoi(s[0])
-			if err != nil {
-				log.Fatal(err)
-			}
-			end, err := strconv.Atoi(s[1])
-			if err != nil {
-				log.Fatal(err)
-			}
-			ranges = append(ranges, idRange{start, end})
-			continue
-		}
-
-		ingredient, err := strconv.Atoi(line)
+		s := strings.Split(line, "-")
+		start, err := strconv.Atoi(s[0])
 		if err != nil {
 			log.Fatal(err)
 		}
+		end, err := strconv.Atoi(s[1])
+		if err != nil {
+			log.Fatal(err)
+		}
+		ranges = append(ranges, &idRange{start, end})
+	}
 
-		for _, r := range ranges {
-			if ingredient >= r.start && ingredient <= r.end {
-				total++
-				break
+	sort.Slice(ranges, func(i, j int) bool {
+		ri, rj := ranges[i], ranges[j]
+		if ri.start < rj.start {
+			return true
+		}
+		if ri.start == rj.start {
+			if ri.end < rj.end {
+				return true
 			}
 		}
+		return false
+	})
+
+	// merge overlapping intervals
+	currIndex := 0
+
+	for i := 1; i < len(ranges); i++ {
+		curr := ranges[currIndex]
+		next := ranges[i]
+		// we know that next.start >= curr.start because of the sort
+
+		if next.start > curr.end {
+			currIndex++
+			ranges[currIndex] = next
+			continue
+		}
+		// either
+		// 1. next is contained within curr
+		// 2. next and curr overlap
+		// 3. they're just kissing
+		// either way, we only change the end of the range
+		curr.end = max(curr.end, next.end)
+	}
+
+	total := 0
+
+	for i := 0; i <= currIndex; i++ {
+		interval := ranges[i]
+		total += interval.end - interval.start + 1
 	}
 
 	return total
@@ -53,4 +76,11 @@ func cafeteria() int {
 type idRange struct {
 	start int
 	end   int
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
 }
