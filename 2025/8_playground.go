96091aa15b61bb07eaad0241b8431e1d677844f1
Louis Heath <lheath1998@gmail.com>
2025-12-22 17:56:15 +0000

    day 8 part 1

--- 2025/8_playground.go
+++ 2025/8_playground.go
@@ -1,50 +1,123 @@
 package main
 
-import "math"
+import (
+	"log"
+	"math"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+const MAX_CONNS = 1000
 
 func playground() int {
-	_ = readFile("8_input.txt")
+	boxes := readFile("8_input.txt")
 
-	/*
-		I need to find the two boxes closest together and join them into the same circuit
-		I need to do this 1000 times
-		I then need to take the three largest circuits and multiply together the number of boxes in each
+	// Parse all string boxes into structs
+	junctionBoxes := make([]*junctionBox, len(boxes))
+	for i, line := range boxes {
+		coords := strings.Split(line, ",")
+		x, err := strconv.Atoi(coords[0])
+		if err != nil {
+			log.Fatal((err))
+		}
+		y, err := strconv.Atoi(coords[1])
+		if err != nil {
+			log.Fatal((err))
+		}
+		z, err := strconv.Atoi(coords[2])
+		if err != nil {
+			log.Fatal((err))
+		}
+		junctionBoxes[i] = &junctionBox{x, y, z}
+	}
 
-		I need a data structure to hold shortest distances
-		and a data structure to hold circuits
-		both could be graphs
+	// Calculate distances between all boxes. O(n^2)
+	possibleConns := []connection{}
+	for i, box1 := range junctionBoxes {
+		for j, box2 := range junctionBoxes {
+			if i == j {
+				// rather than compare everything twice, break halfway
+				break
+			}
+			distance := box1.dist(box2)
+			possibleConns = append(possibleConns, connection{
+				distance, box1, box2,
+			})
+		}
+	}
 
-		Shortest distances
-		O(n2), find the distance between all boxes using pythagoras, create a fully connected graph
-		Also maintain a sorted list of these connections and their distances so that we can easily
-		pull the shortest. Actually, maybe we only need the sorted list.
+	// O(nlogn). Don't bother with binary search inserts
+	sort.Slice(possibleConns, func(i, j int) bool {
+		c1, c2 := possibleConns[i], possibleConns[j]
+		return c1.distance < c2.distance
+	})
 
-		When two boxes are connected, we'll join them in an adjacency list
-		After connecting 1000 boxes, we can explore the adjacent list graph to find the number of created circuits and their sizes
-	*/
+	// Build graph of connected closest junction boxes
+	graph := map[*junctionBox][]*junctionBox{}
+	for i := 0; i < MAX_CONNS && i < len(possibleConns); i++ {
+		conn := possibleConns[i]
 
-	// loop 1 parse all string boxes into structs
+		bs1 := graph[conn.a]
+		bs1 = append(bs1, conn.b)
+		graph[conn.a] = bs1
 
-	// loop 2 double for-each, calculate distances, append to slice
+		bs2 := graph[conn.b]
+		bs2 = append(bs2, conn.a)
+		graph[conn.b] = bs2
+	}
 
-	// loop 3 sort the slice O(nlogn). we've already done an O(n^2). don't bother with binary search insert
+	// Explore the graph to find the sizes of each circuit
+	circuitSizes := []int{}
+	explored := map[*junctionBox]bool{}
 
-	// loop 4 iterate through the first 1000, build graph
+	for b := range graph {
+		if alreadyExplored := explored[b]; alreadyExplored {
+			continue
+		}
+		// bfs this junction box
+		q := []*junctionBox{b}
+		explored[b] = true
+		circuitSize := 1
+		for len(q) > 0 {
+			// dequeue
+			next := q[0]
+			q = q[1:]
 
-	// loop 5 explore the graph, find the sizes of each circuit
+			// explore neighbours
+			ns := graph[next]
+			for _, n := range ns {
+				if alreadyExplored := explored[n]; alreadyExplored {
+					continue
+				}
+				q = append(q, n)
+				explored[n] = true
+				circuitSize++
+			}
+		}
+		circuitSizes = append(circuitSizes, circuitSize)
+	}
 
-	return 1
+	// Find the three largest circuits
+	sort.Ints(circuitSizes)
+
+	return circuitSizes[len(circuitSizes)-1] *
+		circuitSizes[len(circuitSizes)-2] *
+		circuitSizes[len(circuitSizes)-3]
+}
+
+type junctionBox struct {
+	x, y, z int
+}
+
+func (b *junctionBox) dist(a *junctionBox) float64 {
+	x := b.x - a.x
+	y := b.y - a.y
+	z := b.z - a.z
+	return math.Sqrt(float64(x*x + y*y + z*z))
 }
 
 type connection struct {
 	distance float64
-	i        int
-	j        int
-}
-
-func dist(a, b, c, d, e, f int) float64 {
-	x := a - d
-	y := b - e
-	z := c - f
-	return math.Sqrt(float64(x*x + y*y + z*z))
+	a, b     *junctionBox
 }
--- 2025/go.mod
+++ 2025/go.mod
@@ -1,3 +1,5 @@
 module adventofcode/2025
 
 go 1.21.4
+
+require github.com/davecgh/go-spew v1.1.1 // indirect
